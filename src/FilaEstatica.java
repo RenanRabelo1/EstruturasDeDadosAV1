71784c61f5bad198c30eac170ce73e5978dc3bc8
RenanRabelo1 <renanrabelo82@gmail.com>
2026-03-17 09:07:20 -0300

    Completo Fila Estática

---
+++ src/FilaEstatica.java
@@ -0,0 +1,71 @@
+public class FilaEstatica {
+
+
+    private int [] dados;
+    private int inicio;
+    private int fim;
+    private int tamanho;
+
+    public FilaEstatica(int capacidade){
+        dados = new int [capacidade];
+
+         inicio = 0;
+         tamanho = 0;
+            fim = 0;
+    }
+
+    public boolean isEmpty(){ return tamanho == 0;}
+
+
+
+    public boolean EntrarNaFila(int valor){
+
+    if (tamanho == dados.length){
+        System.out.println("Fila está cheia");
+        return false;
+    }
+        dados[fim]=valor;
+
+        fim = (fim + 1) % dados.length;
+        tamanho++;
+        return true;
+    }
+
+    public int SairDaFila(){
+        if (isEmpty()){
+            System.out.println("Fila Vazia");
+            return -1;
+        }
+
+        int valor = dados[inicio];
+        inicio = (inicio+1) % dados.length;
+        tamanho--;
+        return valor;
+    }
+
+    public int peek(){
+        return dados[inicio];
+    }
+
+    public void ListaInteira(){
+
+        for(int i = 0; i < tamanho; i++){
+
+            int indice = (inicio+i) % dados.length;
+            System.out.println(i+1 + "°:" + dados[indice]);
+        }
+        System.out.println();
+    }
+
+    public int Tamanho(){return tamanho;}
+
+    public int Ultimo(){
+        int indiceUltimo = (inicio + tamanho - 1) % dados.length;
+        return dados[indiceUltimo];
+    }
+
+        public int UltimoF(){
+            return dados[fim];
+        }
+
+}
