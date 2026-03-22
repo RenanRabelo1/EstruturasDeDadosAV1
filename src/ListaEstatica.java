8ef0eca9be73807f9f50e2c77a75439f8239d459
Caique Mendonça <132861061+Caique-Mendonca@users.noreply.github.com>
2026-03-22 14:27:01 -0300

    caique parte 2

--- src/ListaEstatica.java
+++ src/ListaEstatica.java
@@ -127,30 +127,5 @@ public class ListaEstatica {
 
         l.imprimir();
 
-        // Buscar numeros na lista
-        l.buscar(10);
-        l.buscar(70);
-
-        l.isEmpty();
-        l.isFull();
-        
-        l.imprimir();
-
-        // Contar elementos
-        System.out.println("Tamanho da lista: " + l.tamanho());
-
-        // Inverter lista
-        l.inverter();
-
-        l.imprimir();
-
-        // Retornar soma
-        System.out.println("Soma dos elementos da lista: " + l.somar());
-
-        // Liberar memória
-        l.esvaziar();
-
-        System.out.println("A lista está vazia após liberar espaço? " + l.isEmpty() );
-
     }
 }
